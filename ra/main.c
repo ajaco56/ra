4ec7a85105982808d562bf929fcc5cf9670a0da1
Andreas Jacobs <ajaco@xs4all.nl>
2022-04-13 14:35:22 +0200

    Initial Commit

---
+++ ra/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ra
+//
+//  Created by CDust Creative Engineering on 13/04/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
